d38413ea68ea59c5f80be9efc438b24738911893
Ernest Salim <salim.ernest17@gmail.com>
2021-09-20 23:22:26 +0700

    refactor: scss structure

--- src/utils/toxicityClassifier.js
+++ src/utils/toxicityClassifier.js
@@ -2,13 +2,13 @@ import '@tensorflow/tfjs';
 
 const toxicity = require('@tensorflow-models/toxicity');
 
-const toxicityTreshold = 0.9;
-const toxicityCategories = ['toxicity', 'severe_toxicity', 'insult', 'threat'];
+const toxicityTreshold = 0.6;
+// const toxicityCategories = ['toxicity', 'severe_toxicity', 'insult', 'threat'];
 
 export default () =>
     new Promise((resolve, reject) => {
         toxicity
-            .load(toxicityTreshold, toxicityCategories)
+            .load(toxicityTreshold)
             .then((res) => {
                 if (res) resolve(res);
                 else reject(res);
