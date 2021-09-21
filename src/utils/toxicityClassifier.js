5a4f3651b9531eb518f5beeaf1dd01a5c306bd90
Jos02378 <57058570+Jos02378@users.noreply.github.com>
2021-09-21 10:10:49 +0700

    feat: fix profile style

--- src/utils/toxicityClassifier.js
+++ src/utils/toxicityClassifier.js
@@ -6,15 +6,15 @@ const toxicityTreshold = 0.6;
 // const toxicityCategories = ['toxicity', 'severe_toxicity', 'insult', 'threat'];
 
 export default () =>
-    new Promise((resolve, reject) => {
-        toxicity
-            .load(toxicityTreshold)
-            .then((res) => {
-                if (res) resolve(res);
-                else reject(res);
-            })
-            .catch((err) => {
-                console.log(err);
-                reject(err);
-            });
-    });
+  new Promise((resolve, reject) => {
+    toxicity
+      .load(toxicityTreshold)
+      .then((res) => {
+        if (res) resolve(res);
+        else reject(res);
+      })
+      .catch((err) => {
+        console.log(err);
+        reject(err);
+      });
+  });
